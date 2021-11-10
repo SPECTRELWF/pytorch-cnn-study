8245c9b4e137447f94960b18409145da0f7a7605
SPECTRELWF <51198441+SPECTRELWF@users.noreply.github.com>
2021-11-10 10:15:18 +0800

    Add files via upload

---
+++ ResNet/resnet50.py
@@ -0,0 +1,22 @@
+# !/usr/bin/python3
+# -*- coding:utf-8 -*-
+# Author:WeiFeng Liu
+# @Time: 2021/11/9 下午4:57
+
+import torchvision
+import torch.nn as nn
+
+
+
+class my_resnet50(nn.Module):
+    def __init__(self):
+        super(my_resnet50, self).__init__()
+        self.backbone = torchvision.models.resnet50(pretrained=True)
+        self.fc2 = nn.Linear(1000,512)
+        self.fc3 = nn.Linear(512,2)
+
+    def forward(self,x):
+        x = self.backbone(x)
+        x = self.fc2(x)
+        x = self.fc3(x)
+        return x
